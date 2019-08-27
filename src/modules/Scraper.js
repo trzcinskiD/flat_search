5145e0dbaa90b8a6aef29211ede0c395b523ba55
Damian Trzciński <damian.trzcinski@cdn.com.pl>
2019-08-27 08:49:17 +0200

    script from site

---
+++ src/modules/Scraper.js
@@ -0,0 +1,24 @@
+// Pobranie danych ze strony i zwrócenie informacji do dalszego przetwarzania
+const puppeteer = require("puppeteer");
+const Config = require("./Config");
+
+module.exports = async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(Config.pageURL);
+  // evaulate uruchamia callbacka zawierającego context strony
+  const fetchedOffers = await page.evaluate(() => {
+    const offers = Array.from(document.querySelectorAll(".offer-wrapper"));
+    const offersTitles = offers.map(offer => {
+      return {
+        title: offer.querySelector("strong").innerText,
+        url: offer.querySelector(".link").getAttribute("href"),
+        price: offer.querySelector(".price strong").innerText,
+        location: offer.querySelector(".breadcrumb").innerText
+      };
+    });
+    return offersTitles;
+  });
+  await browser.close();
+  return fetchedOffers;
+};
