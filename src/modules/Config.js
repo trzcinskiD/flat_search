0298efaa17fcb9b04624c36bf131a7ab39a45806
Damian Trzciński <damian.trzcinski@cdn.com.pl>
2019-08-29 09:01:17 +0200

    my own url added

--- src/modules/Config.js
+++ src/modules/Config.js
@@ -1,6 +1,7 @@
 module.exports = {
   refreshTime: 600000,
-  pageURL: "https://www.olx.pl/nieruchomosci/mieszkania/wynajem/krakow/",
+  pageURL:
+    "https://www.olx.pl/nieruchomosci/mieszkania/wynajem/krakow/?min_id=551516536&search%5Bfilter_float_price%3Ato%5D=1600&search%5Bdist%5D=0&search%5Bfilter_float_m%3Afrom%5D=25",
   mailMessage: {
     from: "trzcinskid0@gmail.com",
     to: "trzcinskid0@gmail.com",
